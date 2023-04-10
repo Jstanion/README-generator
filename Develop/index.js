4efbe7a980a550fdf4c27edd1b95a1ad30dd1200
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-04-09 22:53:56 -0500

    added comments to the index.js file for easier reading

--- Develop/index.js
+++ Develop/index.js
@@ -9,12 +9,12 @@ const generateMarkdown = require('./utils/generateMarkdown');
 function init() {
 inquirer
   .prompt([
-    {
+    { // Title section prompts
       type: 'input',
       name: 'title',
       message: 'What is the title name for your application?',
     },
-    {
+    { // Description section prompts
       type: 'input',
       name: 'description1',
       message: 'What was your motivation for this project?',
@@ -34,7 +34,7 @@ inquirer
       name: 'description4',
       message: 'What did you learn?',
     },
-    {
+    { // Installation section prompts
       type: 'confirm',
       name: 'installation',
       message: 'Are there required steps to install your project?',
@@ -47,12 +47,12 @@ inquirer
       filter: (input) => `${input}`,
       when: (data) => data.installation, // Only displays answer if the installation question is given a truthy value
     },
-    {
+    { // Usage section prompts
       type: 'input',
       name: 'usage',
       message: 'Please provide any usage instructions and examples:',
     },
-    {
+    { // Screenshots section prompts
       type: 'confirm',
       name: 'screenshot',
       message: 'Would you like to add a screenshot?',
@@ -72,7 +72,7 @@ inquirer
       filter: (input) => `(${input})`,
       when: (data) => data.screenshot, // Only displays answer if the installation question is given a truthy value
     },
-    {
+    { // Walkthrough section prompts
       type: 'confirm',
       name: 'walkthrough',
       message: 'Would you like to add a video walkthrough link?',
@@ -92,7 +92,7 @@ inquirer
       filter: (input) => `(${input})`,
       when: (data) => data.walkthrough, // Only displays answer if the installation question is given a truthy value
     },
-    {
+    { // Credit section prompts
       type: 'confirm',
       name: 'credits',
       message: 'Would you like to add a collaborator, third-party asset, or tutorial?',
@@ -112,14 +112,14 @@ inquirer
       filter: (input) => `(${input})`,
       when: (data) => data.walkthrough, // Only displays answer if the installation question is given a truthy value
     },
-    {
+    { // Lisence section prompts
       type: 'list',
       name: 'license',
       message: 'Please choose a license for your application:',
       choices: ['None', 'MIT', 'BSD', 'Apache 2.0', 'Copyleft', 'GNU GPLv3'],
       default: 'None'
     },
-    {
+    { // Contribution section prompts
       type: 'confirm',
       name: 'contributing',
       message: 'Would you like to add the standard contribution guidelines from Contributor Covenant?',
@@ -139,7 +139,7 @@ inquirer
       filter: (input) => `(${input})`,
       when: (data) => !data.contributing, // Only displays answer if the installation question is given a truthy value
     },
-    {
+    { // Test section prompts
       type: 'confirm',
       name: 'tests',
       message: 'Would you like to provide tests for your application?',
@@ -165,7 +165,7 @@ inquirer
       filter: (input) => `(${input})`,
       when: (data) => data.tests, // Only displays answer if the installation question is given a truthy value
     },
-    {
+    { // Questions section prompts
       type: 'input',
       name: 'name',
       message: 'Please provide your full name:',
